c45638bee81e1a56c1fb84c8b0c430ec62f3e7a7
Eugene Klymenko <32247753+SralexeyOvalniy@users.noreply.github.com>
2020-10-08 16:34:54 +0300

    Add files via upload

---
+++ Practice_3.py
@@ -0,0 +1,9 @@
+R = int(input("Please enter your human dog age: "))
+if R == 1 :
+    print("Doge age is 10,5")
+elif R == 2:
+    print("Doge age is 21")
+elif R > 2:
+    print( 21 + R * 4)
+elif R <= 0:
+    print("Error")
